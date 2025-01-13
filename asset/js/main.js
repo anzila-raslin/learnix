11f07d6e00cc2c99b731e0f25cc5f66a434cf5c5
anzila-raslin <anzilaminnu@gmail.com>
2025-01-13 13:34:56 +0530

    modified

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,1 +1,52 @@
+let glowInTexts = document.querySelectorAll(".glowIn");
+glowInTexts.forEach(glowInText => {
+  let letters = glowInText.textContent.split("");
+  glowInText.textContent = "";
+  letters.forEach((letter, i) => {
+    let span = document.createElement("span");
+    span.textContent = letter;
+    span.style.animationDelay = `${i * 0.05}s`;
+    glowInText.append(span);
+  });
+});
+
+// registration
+const container = document.querySelector(".registration"),
+  pwShowHide = document.querySelectorAll(".showHidePw"),
+  pwFields = document.querySelectorAll(".password"),
+  signUp = document.querySelector(".signup-link"),
+  login = document.querySelector(".login-link");
+
+// js code to show/hide password and change icon
+pwShowHide.forEach((eyeIcon) => {
+  eyeIcon.addEventListener("click", () => {
+    pwFields.forEach((pwField) => {
+      if (pwField.type === "password") {
+        pwField.type = "text";
+
+        pwShowHide.forEach((icon) => {
+          icon.classList.replace("uil-eye-slash", "uil-eye");
+        });
+      } else {
+        pwField.type = "password";
+
+        pwShowHide.forEach((icon) => {
+          icon.classList.replace("uil-eye", "uil-eye-slash");
+        });
+      }
+    });
+  });
+});
+
+// appear login and registration form
+signUp.addEventListener("click", (e) => {
+  e.preventDefault();
+  container.classList.add("active");
+});
+
+login.addEventListener("click", (e) => {
+  e.preventDefault();
+  container.classList.remove("active");
+});
+
 
